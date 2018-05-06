856bf962a32d20801d73f86ab025c536e02d85d7
Harpreet <harpreet.vishnoi@gmail.com>
2018-05-07 02:38:15 +0530

    Name changes& comments added

--- Conv_operations_numpy/forward_backward_conv.py
+++ Conv_operations_numpy/forward_backward_conv.py
@@ -1,20 +1,20 @@
 import numpy as np
 
 
-def affine_forward(x, w, b):
+def affine_forward(x, weight, bias):
     """
     Computes the foward pass for an affine layer.
     x:input x
-    w:weight for affine_forward layer
-    b:bias
+    weight:weight for affine_forward layer
+    bias:bias
     out:output of weight,x,bias
     cache:data stored for backward pass
     """
-    out = np.zeros((x.shape[0],w.shape[1]))
+    out = np.zeros((x.shape[0],weight.shape[1]))
 
     x1=np.reshape(x,(x.shape[0],-1))   
-    out=np.dot(x1,w)+b
-    cache = (x, w, b)
+    out=np.dot(x1,weight)+bias
+    cache = (x, weight, bias)
     return out, cache
 
 
@@ -24,17 +24,17 @@ def affine_backward(dout, cache):
     dout:Upstream derivative
     cache:data from affine_forward layer.
     dx:Gradient with respect to x
-    dw:Gradient with respect to w
-    db:Gradient with respect to b
+    dweight:Gradient with respect to weight
+    dbias:Gradient with respect to bias
     """
-    x, w, b = cache
-    dx, dw, db = None, None, None
+    x, weight, bias = cache
+    dx, dweight, dbias = None, None, None
 
-    dx=np.dot(dout,w.T)
+    dx=np.dot(dout,weight.T)
     dx=np.reshape(dx,x.shape)
-    dw=np.dot(np.reshape(x,(x.shape[0],np.prod(x.shape[1:]))).T,dout)
-    db=np.sum(dout,axis=0)
-    return dx, dw, db
+    dweight=np.dot(np.reshape(x,(x.shape[0],np.prod(x.shape[1:]))).T,dout)
+    dbias=np.sum(dout,axis=0)
+    return dx, dweight, dbias
 
 
 def relu_forward(x):
@@ -60,13 +60,13 @@ def relu_backward(dout, cache):
 
     return dx
 
-def conv_forward_naive(x, w, b, conv_param):
+def conv_forward_naive(x, weight, bias, conv_param):
 
     """
     An implementation of the forward pass for a convolutional layer. 
     x:input 
-    w:convolutional filter weights
-    b:convolutional filter bias
+    weight:convolutional filter weights
+    bias:convolutional filter bias
     conv_param:dictionary containing padding and striding for the filters
   
     """
@@ -76,10 +76,10 @@ def conv_forward_naive(x, w, b, conv_param):
     x_pad=np.pad(x,((0,0),(0,0),(p,p),(p,p)), 'constant')
 
     (N, C, H, W)=x.shape
-    (F, C, hh, ww)=w.shape	
+    (F, C, filter_len, filter_width)=weight.shape	
    
-    out_r=int((H-hh+2*p)/s)+1#formula for calcuating the output:    int(input-filter_size+2*padding)+1
-    out_c=int((W-ww+2*p)/s)+1
+    out_r=int((H-filter_len+2*p)/s)+1#formula for calcuating the output:    int(input-filter_size+2*padding)+1
+    out_c=int((W-filter_width+2*p)/s)+1
    
     out=np.zeros((N,F,out_r,out_c))
     
@@ -87,19 +87,19 @@ def conv_forward_naive(x, w, b, conv_param):
  
     
       
-  for nn in range(N):
+  for num_batches in range(N):
       
-       for ff in range(F):
+       for num_channels in range(F):
             
-           for ii in range(0,out_c):#selecting the width(rows wise) of x
+           for num_row in range(0,out_c):#selecting the width(rows wise) of x
               
-               for jj in  range(0,out_r):#selecting the length(column wise) of x
+               for num_col in  range(0,out_r):#selecting the length(column wise) of x
                     
-                    out[nn,ff,ii,jj]=np.sum(x_pad[nn, :, ii*s:ii*s+hh, jj*s:jj*s+ww]*w[ff,...])+b[ff]     
+                    out[num_batches,num_channels,num_row,num_col] = np.sum(x_pad[num_batches, :, num_row*s:num_row*s+filter_len, num_col*s:num_col*s+filter_width]*weight[num_channels,...]) + bias[num_channels]     
                     #padded x multiplied with weights
    
 
-    cache = (x, w, b, conv_param)
+    cache = (x, weight, bias, conv_param)
     return out, cache
 
 
@@ -110,44 +110,44 @@ def conv_backward_naive(dout, cache):
     """
     An implementation of the backward pass for a convolutional layer. 
     dout:upstream derivatives 
-    cache:data from forward conv layer which helps to calculate dx,dw,db
+    cache:data from forward conv layer which helps to calculate dx,dweight,dbias
     dx:Gradient with respect to x
-    dw:Gradient with respect to w
-    db:Gradient with respect to b
+    dweight:Gradient with respect to weight
+    dbias:Gradient with respect to bias
  
     """
    
 
-    (x, w, b, conv_param)=cache
+    (x, weight, bias, conv_param)=cache
     p=conv_param['pad']
     s=conv_param['stride']
     
     dx=np.zeros((x.shape))
-    dw=np.zeros((w.shape))
+    dweight=np.zeros((weight.shape))
 
     x_pad=np.pad(x,((0,0),(0,0),(p,p),(p,p)), 'constant')
  
    
     N,F,H,W=dout.shape
-    _,_,hh,ww=w.shape
+    _,_,filter_len,filter_width=weight.shape
    
    
-    for nn in range(N):
+    for num_batches in range(N):
  
-        for ff in range(F):
+        for num_channels in range(F):
 
-            for ii in range(H):
+            for num_row in range(H):
  
-                for jj in range(W):
+                for num_col in range(W):
 
-                     dw[ff,...]+=x_pad[nn,:,ii*s:ii*s+hh,jj*s:jj*s+ww] * dout[nn,ff,ii,jj]
+                     dweight[num_channels,...]+ = x_pad[num_batches,:,num_row*s:num_row*s+filter_len,num_col*s:num_col*s+filter_width] * dout[num_batches,num_channels,num_row,num_col]
 
-                     db[ff,]+= dout[nn,ff,ii,jj]
+                     dbias[num_channels,]+ = dout[num_batches,num_channels,num_row,num_col]
                      
-                     dx[nn,:,ii*s:ii*s+hh,jj*s:jj*s+ww]+=w[ff,...]*dout[nn,ff,ii,jj]
+                     dx[num_batches,:,num_row*s:num_row*s+filter_len,num_col*s:num_col*s+filter_width]+ = weight[num_channels,...]*                      dout[num_batches,num_channels,num_row,num_col]
 
 
     dx=dx[:,:,p:-p,p:-p]#dx calcualated in for loop is for dx_pad,it has to be reduced to x i.e. removing the padding
        
-    return dx, dw, db
+    return dx, dweight, dbias
 
